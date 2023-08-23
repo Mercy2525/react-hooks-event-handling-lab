d2224ad55fc8644cd134d6712a3cfd19fbcbc18e
ASUS <you@example.com>
2023-08-23 20:22:11 +0300

    tests passed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+
+    const toFocus=()=> console.log("Good!");
+    const toBlur=()=>console.log("Hey! Eyes on me!")
+    
+    return(<div>
+        <button onFocus={toFocus} onBlur={toBlur}>Eyes on me</button>
+    </div>)
+
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,12 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    const enterPass=()=>console.log("Entering password...");
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={enterPass} />
+        </div>
     )
 }
 
